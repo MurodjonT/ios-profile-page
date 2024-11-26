e2f0a0dc47be16062914ca676c4bd81e59e52942
MurodjonT <murodjon.mmm.99@mail.ru>
2024-11-26 08:38:23 +0500

    Initial Commit

---
+++ ios-profile-page/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-profile-page
+//
+//  Created by Murodjon Turobov on 26/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
